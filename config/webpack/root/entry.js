50a9136a0203385e6e63dccec358b2a0b8fc95ee
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-06-26 11:41:29 +0200

    Force vendors to be loaded from module home.

--- config/webpack/root/entry.js
+++ config/webpack/root/entry.js
@@ -1,9 +1,12 @@
+const { moduleHome } = require('../../')
+const path = require('path')
+
 module.exports = () => ({
 	vendor: [
-		'formdata-polyfill',
-		'babel-polyfill',
-		'whatwg-fetch',
-		'url-search-params-polyfill'
+		path.resolve(moduleHome, 'node_modules', 'formdata-polyfill'),
+		path.resolve(moduleHome, 'node_modules', 'babel-polyfill'),
+		path.resolve(moduleHome, 'node_modules', 'whatwg-fetch'),
+		path.resolve(moduleHome, 'node_modules', 'url-search-params-polyfill')
 	],
 	app : [
 		'./src'
