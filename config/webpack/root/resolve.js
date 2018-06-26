9fd2f1730d7d993a1e7f14a9f9f489879573bed3
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-06-26 12:40:15 +0200

    Correctly resolve from modules dir also

--- config/webpack/root/resolve.js
+++ config/webpack/root/resolve.js
@@ -3,5 +3,9 @@ const path = require('path')
 
 module.exports = () => ({
 	extensions : ['.js', '.mjs', '.css', '.scss'],
-	modules: ['node_modules', path.resolve(moduleHome, 'node_modules')]
+	modules: ['node_modules', path.resolve(moduleHome, 'node_modules')],
+	resolveLoader: {
+		modules: ['node_modules', path.resolve(moduleHome, 'node_modules')],
+		extensions: ['.js', '.json']
+	}
 })
