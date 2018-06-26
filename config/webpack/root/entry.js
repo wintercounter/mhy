86b3fb3560f635b01850a01eeacfb7d7d95a3fc3
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-06-26 11:54:16 +0200

    Correctly resolve from modules dir also

--- config/webpack/root/entry.js
+++ config/webpack/root/entry.js
@@ -1,12 +1,9 @@
-const { moduleHome } = require('../../')
-const path = require('path')
-
 module.exports = () => ({
 	vendor: [
-		path.resolve(moduleHome, 'node_modules', 'formdata-polyfill'),
-		path.resolve(moduleHome, 'node_modules', 'babel-polyfill'),
-		path.resolve(moduleHome, 'node_modules', 'whatwg-fetch'),
-		path.resolve(moduleHome, 'node_modules', 'url-search-params-polyfill')
+		'formdata-polyfill',
+		'babel-polyfill',
+		'whatwg-fetch',
+		'url-search-params-polyfill'
 	],
 	app : [
 		'./src'
--- config/webpack/root/resolve.js
+++ config/webpack/root/resolve.js
@@ -1,3 +1,7 @@
+const { moduleHome } = require('../../')
+const path = require('path')
+
 module.exports = () => ({
-	extensions : ['.js', '.mjs', '.css', '.scss']
+	extensions : ['.js', '.mjs', '.css', '.scss'],
+	modules: ['node_modules', path.resolve(moduleHome, 'node_modules')]
 })
