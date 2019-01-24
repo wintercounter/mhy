61924393024758196939b209524bf3de2cbaca4c
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-01-24 11:01:21 +0100

    fix(babel): only resolve module if it's not being run by Webpack

--- src/configs/babel/root/plugins.js
+++ src/configs/babel/root/plugins.js
@@ -1,34 +1,38 @@
-import fs from 'fs'
-import path from 'path'
 import mhyConfig from '@/configs/mhy'
 
-export default (defaults = []) => [
-    ...defaults,
-    require.resolve('babel-plugin-macros'),
-    require.resolve('@babel/plugin-syntax-dynamic-import'),
-    require.resolve('babel-plugin-transform-remove-strict-mode'),
-    require.resolve('@babel/plugin-proposal-class-properties'),
-    require.resolve('@babel/plugin-transform-object-assign'),
-    [
-        require.resolve('@babel/plugin-syntax-decorators'),
-        {
-            legacy: true
-        }
-    ],
-    require.resolve('babel-plugin-syntax-async-functions'),
-    require.resolve('@babel/plugin-transform-regenerator'),
-    require.resolve('babel-plugin-transform-function-bind'),
-    require.resolve('@babel/plugin-proposal-export-default-from'),
-    require.resolve('@babel/plugin-proposal-export-namespace-from'),
-    [
-        require.resolve('babel-plugin-module-resolver'),
-        {
-            root: [],
-            alias: Object.entries(mhyConfig.defaultAliases).reduce(function(acc, [k]) {
-                const isDist = fs.existsSync(path.resolve(process.cwd(), 'dist'))
-                acc[k] = k.replace('@', `./${isDist ? 'dist' : 'src'}/`)
-                return acc
-            }, {})
-        }
+export default (defaults = []) => {
+    const r = [
+        ...defaults,
+        require.resolve('babel-plugin-macros'),
+        require.resolve('@babel/plugin-syntax-dynamic-import'),
+        require.resolve('babel-plugin-transform-remove-strict-mode'),
+        require.resolve('@babel/plugin-proposal-class-properties'),
+        require.resolve('@babel/plugin-transform-object-assign'),
+        [
+            require.resolve('@babel/plugin-syntax-decorators'),
+            {
+                legacy: true
+            }
+        ],
+        require.resolve('babel-plugin-syntax-async-functions'),
+        require.resolve('@babel/plugin-transform-regenerator'),
+        require.resolve('babel-plugin-transform-function-bind'),
+        require.resolve('@babel/plugin-proposal-export-default-from'),
+        require.resolve('@babel/plugin-proposal-export-namespace-from')
     ]
-]
+    // Webpack is resolving modules on it's own
+    if (!process.argv.some(v => v.includes('webpack'))) {
+        r.push([
+            require.resolve('babel-plugin-module-resolver'),
+            {
+                root: [],
+                alias: Object.entries(mhyConfig.defaultAliases).reduce(function(acc, [k]) {
+                    // If it's compiled directly with babel, use dist folder
+                    acc[k] = k.replace('@', `./${process.argv.some(v => v.includes('babel')) ? 'dist' : 'src'}/`)
+                    return acc
+                }, {})
+            }
+        ])
+    }
+    return r
+}
