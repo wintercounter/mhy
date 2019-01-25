3cb8adcf7218491084bf1bfb18a532a21e37128e
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-01-25 11:17:04 +0100

    fix(jest): transform modules with export also

--- src/configs/jest/preprocess.js
+++ src/configs/jest/preprocess.js
@@ -1,18 +1,15 @@
 import path from 'path'
 import fs from 'fs'
 
-const babelrc = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, '.babelrc'), 'utf8')
-)
-babelrc.presets.find(([name]) => name.includes('preset-env'))[1].modules =
-    'commonjs'
+const babelrc = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.babelrc'), 'utf8'))
+babelrc.presets.find(([name]) => name.includes('preset-env'))[1].modules = 'commonjs'
 babelrc.plugins.push(require.resolve('babel-plugin-dynamic-import-node'))
 babelrc.plugins.push(require.resolve('babel-plugin-add-module-exports'))
 const transformer = require('babel-jest').createTransformer(babelrc)
 const proc = transformer.process.bind(transformer)
 transformer.process = function(...args) {
     const [code, path] = args
-    if (!path.includes('node_modules') || code.includes('import')) {
+    if (!path.includes('node_modules') || code.match(/^(import|export)|import\(/gm)) {
         return proc(...args)
     }
     return code
