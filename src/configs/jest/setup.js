e5c46e547d444dd53d7b039aba2ee5eff7002765
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-02-13 12:16:48 +0100

    feat(jest): add and configure `jest-date-mock`

--- src/configs/jest/setup.js
+++ src/configs/jest/setup.js
@@ -3,6 +3,7 @@ import Enzyme, { shallow, render, mount } from 'enzyme'
 import 'jest-enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import 'jest-canvas-mock'
+import 'jest-date-mock'
 import 'jest-styled-components'
 import fetch from 'jest-fetch-mock'
 
