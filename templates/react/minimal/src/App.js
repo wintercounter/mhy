fd9944706f06267ed46a1136c023ac19b285ab59
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-02-19 15:24:33 +0100

    feat(boot): add minimal react template

---
+++ templates/react/minimal/src/App.js
@@ -0,0 +1,3 @@
+const App = () => <div>Hello mhy!</div>
+
+export default App
---
+++ templates/react/minimal/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const root = document.querySelector('#root')
+
+ReactDOM.render(<App />, root)
