52d7f02b0b0a57c42dd684bc9e9f9902489ae04c
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-02-19 16:19:58 +0100

    minor(boot): add default styles

--- templates/react/minimal/src/App.js
+++ templates/react/minimal/src/App.js
@@ -1,3 +1,12 @@
-const App = () => <div>Hello mhy!</div>
+import React from 'react'
+
+const styles = {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'center',
+    justifyContent: 'center'
+}
+
+const App = () => <div style={styles}>Hello mhy!</div>
 
 export default App
--- templates/react/minimal/src/index.js
+++ templates/react/minimal/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import App from './App'
+
 const root = document.querySelector('#root')
 
 ReactDOM.render(<App />, root)
