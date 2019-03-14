aa36c4eb077116e5e3bde49f286eea3c12a99fe0
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-03-14 17:16:22 +0100

    feat(debug): add support for `SpeedMeasurePlugin` for `Webpack` behind the `--mhy-debug` flag

--- src/configs/webpack/index.js
+++ src/configs/webpack/index.js
@@ -2,6 +2,13 @@ import { loadConfig } from '@/utils'
 
 process.env.WEBPACK_DEV_SERVER = process.argv.find(v => v.includes('webpack-dev-server'))
 
-const webpackConfig = loadConfig('webpack', {})
+let webpackConfig = loadConfig('webpack', {})
+
+if (process.env.MHY_DEBUG) {
+    const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+    const smp = new SpeedMeasurePlugin()
+
+    webpackConfig = smp.wrap(webpackConfig)
+}
 
 export default webpackConfig
