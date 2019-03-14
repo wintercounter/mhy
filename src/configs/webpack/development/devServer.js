0dc85170d07cbb7c676f13cedbeb97a5d90ec6b9
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-03-14 17:14:16 +0100

    feat(wds): disable dot rule

--- src/configs/webpack/development/devServer.js
+++ src/configs/webpack/development/devServer.js
@@ -4,6 +4,8 @@ export default () => ({
     port: 3000,
     hot: true,
     inline: true,
-    historyApiFallback: true,
+    historyApiFallback: {
+        disableDotRule: true
+    },
     disableHostCheck: true
 })
