1c4a87ac1a5b62d350417e679cdff3d330222406
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-03-18 16:57:44 +0100

    perf(storybook): improve performance by removing eslint check

--- src/configs/storybook/.storybook/webpack.config.js
+++ src/configs/storybook/.storybook/webpack.config.js
@@ -8,6 +8,8 @@ export default ({ config }) => {
     }
     config.resolve = mhyWP.resolve
     config.module = mhyWP.module
+    const eslint = config.module.rules.find(({ loader }) => loader.includes('eslint'))
+    config.module.rules.splice(config.module.rules.indexOf(eslint), 1)
     config.mode = 'development'
 
     return config
