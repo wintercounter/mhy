71f883ec9695598798e879b9183a027a3dad6ddf
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-03-26 16:56:40 +0100

    fix(webpack-eslint): only set options for prod if rule exists

--- src/configs/webpack/production/module/rules/eslint.js
+++ src/configs/webpack/production/module/rules/eslint.js
@@ -1,9 +1,10 @@
 export default rules => {
     const rule = rules.find(({ loader = '' }) => loader.includes('eslint-loader'))
-    rule.options = {
-        ...rule.options,
-        failOnWarning: true,
-        failOnError: true
-    }
+    rule &&
+        (rule.options = {
+            ...rule.options,
+            failOnWarning: true,
+            failOnError: true
+        })
     return rules
 }
