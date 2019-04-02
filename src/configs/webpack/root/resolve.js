300e2deefa0da523c78fb7a4c104e179d6762c98
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-04-02 11:08:41 +0200

    feat(webpack): make alias resolve more reliable

--- src/configs/webpack/root/resolve.js
+++ src/configs/webpack/root/resolve.js
@@ -1,11 +1,20 @@
+import fs from 'fs'
 import path from 'path'
 
 import mhyConfig from '@/configs/mhy'
+
+const alias = { ...mhyConfig.defaultAliases }
+for (const [key, entry] of Object.values(alias)) {
+    const p = path.resolve(process.cwd(), entry)
+    if (fs.existsSync(p)) {
+        alias[key] = p
+    }
+}
 
 export default () => ({
     extensions: ['.js', '.mjs', '.jsx', '.css', '.scss', '.ts', '.tsx', '.json'],
     modules: Array.from(
         new Set([path.resolve(__dirname, '../../../../node_modules'), path.resolve(process.cwd(), 'node_modules')])
     ),
-    alias: mhyConfig.defaultAliases
+    alias
 })
