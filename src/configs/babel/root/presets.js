3c499160525f8510065de4d8aba6bf67b1b99bcf
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-07-10 11:10:49 +0200

    chore(dependencies): update 10/07/19

--- src/configs/babel/root/presets.js
+++ src/configs/babel/root/presets.js
@@ -4,8 +4,8 @@ export default (defaults = []) => [
         require.resolve('@babel/preset-env'),
         {
             targets: {
-                "chrome": 78,
-                "edge": 18,
+                chrome: 78,
+                edge: 18,
                 esmodules: false
             },
             modules: false
