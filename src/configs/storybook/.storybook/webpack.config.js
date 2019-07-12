8bbfcff717b1c1020e76b51f13632a27c4d52a0a
Viktor Vincze <viktor.vincze@doclerholding.com>
2019-07-12 11:22:12 +0200

    fix(storybook): only remove eslint rules if found

--- src/configs/storybook/.storybook/webpack.config.js
+++ src/configs/storybook/.storybook/webpack.config.js
@@ -9,7 +9,9 @@ export default ({ config }) => {
     config.resolve = mhyWP.resolve
     config.module = mhyWP.module
     const eslint = config.module.rules.find(({ loader }) => loader && loader.includes('eslint'))
-    config.module.rules.splice(config.module.rules.indexOf(eslint), 1)
+    if (eslint) {
+        config.module.rules.splice(config.module.rules.indexOf(eslint), 1)
+    }
     config.mode = 'development'
 
     return config
