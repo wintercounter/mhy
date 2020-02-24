f4ab985db82910b93778304e0a91240eb490f690
wintercounter <wintercounter@gmail.com>
2020-02-24 21:44:48 +0100

    feat(config): add support for `-p, --path` to be able to print out a certain path only

--- src/processes/command/config.js
+++ src/processes/command/config.js
@@ -5,19 +5,20 @@ import FileTypes from '@/utils/fileTypes'
 import figures from 'figures'
 import chalk from 'chalk'
 
-const commandHandler = ({ config, dir, format, overwrite, init }) => {
+const commandHandler = (args) => {
+    const { config, init } = args
     if (
         !config.length || // default init
         config.length > 1 || // specified init
         init // forced init
     ) {
-        handleWrite(config, dir, format, overwrite)
+        handleWrite(args)
     } else {
-        handlePrint(config, dir, format, overwrite)
+        handlePrint(args)
     }
 }
 
-const handleWrite = (config, dir, format, overwrite) => {
+const handleWrite = ({ config, dir, format, overwrite }) => {
     const m = fetchWriters(config)
     const modules = m.map((module, i) => {
         process.stdout.write(`\rLoading configs ${i + 1}/${m.length}`)
@@ -60,9 +61,14 @@ const handleWrite = (config, dir, format, overwrite) => {
     }
 }
 
-const handlePrint = (config, dir, format) => {
+const applyPath = (path, configObj) => {
+    if (!path) return configObj
+    return path.split('.').reduce((acc, p) => acc[p], configObj)
+}
+
+const handlePrint = ({ config, format, path }) => {
     const module = require(fetchConfigs(config)[0])
-    const configObj = module.default || module
+    const configObj = applyPath(path, module.default || module)
     let formatted = formatSource(configObj, format)
     if (format === FileTypes.JSON) {
         formatted = require('json-colorizer')(formatted)
@@ -116,6 +122,12 @@ export default () => {
                     default: undefined,
                     description: 'Output format',
                     choices: Object.values(FileTypes)
+                })
+                .option('path', {
+                    alias: 'p',
+                    default: undefined,
+                    description: 'You can narrow down the output to a given path. This only works while printing the output the console. Eg: loaders.0.use',
+                    type: 'string'
                 })
         },
         commandHandler
