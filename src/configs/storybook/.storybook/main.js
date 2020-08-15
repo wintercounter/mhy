2fe578a505fe033ae1efc619872555a7a82e2028
Viktor Vincze <viktor.vincze@doclerholding.com>
2020-08-15 23:04:55 +0200

    feat(storybook): general improvements

--- src/configs/storybook/.storybook/main.js
+++ src/configs/storybook/.storybook/main.js
@@ -1,13 +1,5 @@
-import path from 'path'
-
 import mhyWP from '@/configs/webpack'
 import createCompiler from '@storybook/addon-docs/mdx-compiler-plugin'
-import { configure } from '@storybook/react'
-
-/*const setSrcFolder = () => {
-    const data = fs
-        .readFileSync(path.resolve(__dirname, '_config.js'), 'utf8')
-        .replace(/'src'/g, JSON.stringify(path.resolve(process.cwd(), mhyConfig.srcFolder)))*/
 
 const baseWebpackConfig = config => {
     mhyWP.resolve.modules = [...config.resolve.modules, ...mhyWP.resolve.modules, process.cwd()]
@@ -16,30 +8,11 @@ const baseWebpackConfig = config => {
         ...config.resolve.alias
     }
     config.resolve = mhyWP.resolve
+    config.resolveLoader = mhyWP.resolveLoader
     config.module = mhyWP.module
-
-    /* // Transpile css from anywhere
-     config.module.rules.forEach(rule => {
-         if (rule.test.toString().includes('css')) {
-             rule.include = /./
-         }
-     })
-
-     // No need to do eslint here...
-     const eslint = config.module.rules.find(({ loader }) => loader && loader.includes('eslint'))
-     if (eslint) {
-         config.module.rules.splice(config.module.rules.indexOf(eslint), 1)
-     }*/
-    /*config.mode = 'development'*/
 
     return config
 }
-
-/*// Require all *.story.js file
-const req = require.context('@', true, /\.?(story|stories|book)\.([jt]sx?|mdx)$/)
-configure(req, module)*/
-
-console.log(path.normalize(process.cwd()))
 
 export default {
     //stories: [`**/story.tsx`],
--- src/configs/webpack/root/resolveLoader.js
+++ src/configs/webpack/root/resolveLoader.js
@@ -2,7 +2,7 @@ import path from 'path'
 
 const resolveLoader = () => ({
     modules: ['node_modules', path.resolve(__dirname, '../../../../node_modules')],
-    extensions: ['.js', '.mjs', '.css', '.scss']
+    extensions: ['.js', '.mjs', '.css', '.scss', '.json']
 })
 
 export default resolveLoader
