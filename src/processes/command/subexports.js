557b686937847832bc8d78a2dcb34f6535c923c8
Viktor Vincze <viktor.vincze@doclerholding.com>
2021-01-07 13:26:18 +0100

    feat(subexports): add support for `force` flag

--- src/processes/command/subexports.js
+++ src/processes/command/subexports.js
@@ -2,12 +2,14 @@ import path from 'path'
 import yargs from 'yargs'
 import fse from 'fs-extra'
 
-const commandHandler = () => {
-    const args = require('@/configs/subexports')
-    const root = fse.readJsonSync('./package.json').name
+const commandHandler = (args) => {
+    const config = require('@/configs/subexports')
+    const root = require(path.join(process.cwd(), './package.json')).name
 
-    Object.entries(args)
+    Object.entries(config)
         .filter(([pkg, options]) => {
+            if (args.force) return true
+
             const isExists = fse.existsSync(path.resolve(process.cwd(), pkg, options.main))
             !isExists && console.error(`Could not find dist for ${pkg} package`)
             process.exit(1)
@@ -24,5 +26,12 @@ const commandHandler = () => {
 
 export default () => {
     yargs
-        .command('subexports', 'exporting optional sub packages', () => {}, commandHandler)
+        .command(['subexports', 'se'], 'exporting optional sub packages', (y) => {
+            return y.option('f', {
+                alias: 'force',
+                default: false,
+                describe: 'Forcing will skipp checking if dist file exists or not.',
+                type: 'boolean'
+            })
+        }, commandHandler)
 }
