ff4fc7c2977689b62abc5f10d7054efe26131618
viktor.vincze <wintercounter@gmail.com>
2022-12-08 12:42:51 +0100

    fix: remove unnecessary hot-loader when using devServer

--- src/configs/webpack/root/entry.js
+++ src/configs/webpack/root/entry.js
@@ -2,8 +2,5 @@ export default () => {
     const returnValue = {
         app: ['./src']
     }
-    if (process.env.WEBPACK_DEV_SERVER) {
-        returnValue.app = [require.resolve('react-hot-loader/patch'), ...returnValue.app]
-    }
     return returnValue
 }
